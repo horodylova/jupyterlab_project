3dc95863087b06d2f972ef6505cf320bf69cabae
Svitlana Horodylova <horodylova.sv@gmail.com>
2025-02-13 11:53:15 +0000

    Added files: script.py, notes.txt and README.md

---
+++ script.py
@@ -0,0 +1,39 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "991ead15-b80a-403c-877b-89ea9862fc60",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def greet(name):\n",
+    "    return f\"Hello, {name}!\"\n",
+    "\n",
+    "if __name__ == \"__main__\":\n",
+    "    print(greet(\"JupyterLab\"))"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
